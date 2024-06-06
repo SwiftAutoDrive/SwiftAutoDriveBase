2b0530d68819a261c5d0d30ffbe172759c5a9bee
Taiga Takano <ttttghghnb554z@outlook.jp>
2024-06-07 01:44:17 +0900

    first commit

---
+++ SwiftAutoDriveBase/SwiftAutoDriveBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAutoDriveBaseApp.swift
+//  SwiftAutoDriveBase
+//
+//  Created by Taiga Takano on 2024/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAutoDriveBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
